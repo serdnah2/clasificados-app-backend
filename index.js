e7c2e9635d4f865cdf47455643765769d0bb871c
Carlos Cataño <serdnah2@gmail.com>
2021-08-07 08:06:37 -0500

    Using process port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(function (req, res, next) {
 })
 app.use('/api', router);
 
-app.listen(3306, () => {
+app.listen(process.env.PORT || 3306, () => {
     connection.connect((err) => {
         if (err) {
           console.error('error connecting: ' + err.stack);

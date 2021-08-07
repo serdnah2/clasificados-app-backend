18601841cb4d6ab23cd5543b90b088c7d26c0ed0
Carlos Cataño <serdnah2@gmail.com>
2021-08-07 08:30:40 -0500

    Updating db connection data

--- db/dbconnector.js
+++ db/dbconnector.js
@@ -1,10 +1,10 @@
 var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'CLASIFICADOSAPP'
+  host     : 'remotemysql.com', //localhost
+  user     : 'WQfNpo5m9d', //root
+  password : '02KoeuhBea', //''
+  database : 'WQfNpo5m9d' //CLASIFICADOSAPP
 });
 
 exports.connection = connection;

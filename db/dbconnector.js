4b8d90b32a9ab004841896a13a82a4c85ba38d67
Carlos Cataño <serdnah2@gmail.com>
2022-09-10 07:13:09 -0500

    updating database

--- db/dbconnector.js
+++ db/dbconnector.js
@@ -2,9 +2,9 @@ var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
   host     : 'remotemysql.com', //localhost
-  user     : 'WQfNpo5m9d', //root
-  password : '02KoeuhBea', //''
-  database : 'WQfNpo5m9d' //CLASIFICADOSAPP
+  user     : 'iE4Lgju342', //root
+  password : '9FpYZah9wM', //''
+  database : 'iE4Lgju342' //CLASIFICADOSAPP
 });
 
 exports.connection = connection;
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(function (req, res, next) {
 })
 app.use('/api', router);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     connection.connect((err) => {
         if (err) {
           console.error('error connecting: ' + err.stack);
